218a4dfe6f75e46ce63c0f445496926f37e7b93b
kenneth david <kennethdavid256.com>
2024-09-19 23:37:36 +0300

    Initial commit

--- src/components/pages/Cart/Cart.jsx
+++ src/components/pages/Cart/Cart.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Product from '../../Product/Product'
 
 
 const Cart = () => {
@@ -12,7 +14,25 @@ const Cart = () => {
   console.log(cart, totalCart)
   return (
     <div>
-      cart
+      {
+        cart && cart.length > 0 ? <div className='min-h-[80vh] grid sm:grid-cols-2 mx-w-6xl mx-auto'>
+          <div className='flex flex-col justify-center items-center p-3'>{
+          cart.map(item => {
+            return <Product product={item}  key={item.id} />
+          })
+        }</div>
+        <div className='flex flex-col justify-center items-end p-5 space-y-5 mt-14'>
+          <h1 className='font-bold text-lg  text-red-800'>Your cart Summary</h1>
+          <p><span className='text-gray-800 font-bold'>Total items: </span><span>{cart?.length}</span></p>
+          <p><span className='text-gray-800 font-bold'>Total Amount: </span><span>{totalCart}</span></p>
+        </div>
+        </div> : <div className='h-[80vh] flex flex-col justify-center items-center  '>
+          <h1 className='text-gray-800 mb-2 text-2xl font-bold'>Cart is empty</h1>
+          <Link to={'/'}>
+          <button className='bg-red-950 text-white border-2 rounded-lg font-bold p-4'>SHOP NOW</button>
+          </Link>
+          </div>
+      }
     </div>
   )
 }
