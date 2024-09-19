dfe4c58a823a77b2fcbdeadff7e38f7c568d9130
kenneth david <kennethdavid256.com>
2024-09-19 21:39:06 +0300

    Initial commit

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,11 +1,11 @@
 const { configureStore } = require("@reduxjs/toolkit");
 import cartReducer from './cart-slice'
-import dummyReducer from 'dummy-slice'
+
 
 const store = configureStore({
     reducer: {
         cart: cartReducer,
-        dummy: dummyReducer
+       
     }
 })
 
